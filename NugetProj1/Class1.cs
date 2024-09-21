b9f8cc17f2d3c7ff7fbda6e9177389e81fa72da7
Chinedu Adabanya <chinedu.adabanya@leatherback.co>
2024-09-21 21:19:28 +0100

    Add project files.

---
+++ NugetProj1/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace NugetProj1
+{
+    public class Class1
+    {        public void Main()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
---
+++ NugetProj2/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace NugetProj2
+{
+    public class Class1
+    {
+
+    }
+}
